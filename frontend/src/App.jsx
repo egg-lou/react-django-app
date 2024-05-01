6e63af05f1454a2b73ea10d57a6d1f93bd4f0e40
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-01 19:58:54 +0800

    feat: form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import react from "react";
+import { BrowserRouter, Routes, Route, Router, Navigate } from "react-router-dom";
+import { Login } from "./pages/Login.jsx";
+import { Register } from "./pages/Register.jsx";
+import { Home } from "./pages/Home.jsx";
+import { NotFound } from "./pages/NotFound.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />
+}
 
 function App() {
 
   return (
-    <>
-
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/logout' element={<Logout />}/>
+          <Route path='/register' element={<RegisterAndLogout />}/>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
